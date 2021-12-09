32f5df69a118c167ad6ae4966a5198da7fe2e5c9
b-0n3 <lalouabd123@gmail.com>
2021-12-09 13:51:44 +0100

    init

---
+++ src/main/java/com/sichool/project/SichoolBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sichool.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SichoolBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SichoolBackEndApplication.class, args);
+	}
+
+}
